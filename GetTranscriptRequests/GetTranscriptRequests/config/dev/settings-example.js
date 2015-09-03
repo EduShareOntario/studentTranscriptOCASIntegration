61dd1deaa6c5cbc60e459c2620ece7c260d88920
mdwmax <mikewestbrooke@hotmail.com>
2015-09-03 09:56:17 -0400

    Initial commit of code
    Connect to Ocas - get auth token
    get transcript requests
    get transcript details

---
+++ GetTranscriptRequests/GetTranscriptRequests/config/dev/settings-example.js
@@ -0,0 +1,10 @@
+﻿// you need to create a settings.js in both the dev and prod folders under the config node
+// and insert the values for your institution
+module.exports = {
+    userName : 'yourusernamehere',
+    passWord : 'yourpasswordhere',
+    grantType : 'yourgrantypehere',
+    loginUrl: 'https://etms.uat.ocas.ca/api/auth/login',
+    requestsNoResponseUrl : 'https://etms.uat.ocas.ca/api/transcriptrequests/no_response',
+    transcriptRequestUrl : 'https://etms.uat.ocas.ca/api/transcriptrequests/'
+}
