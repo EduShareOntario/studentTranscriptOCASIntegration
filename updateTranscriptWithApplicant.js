47052a97d58f604ddb799fb1759d6a5490081a36
Todd Hiles <todd.hiles@georgiancollege.ca>
2015-11-21 20:18:01 -0500

    Fetch applicant from Banner DB. - still a work-in-progress.

--- updateTranscriptWithApplicant.js
+++ updateTranscriptWithApplicant.js
@@ -9,13 +9,11 @@ var config = require('app-config');
 var later = require('later');
 var _ = require('underscore');
 var db = require('oracledb');
-var Fiber = require('fibers');
-var Future = require('fibers/future');
+//var Fiber = require('fibers');
+//var Future = require('fibers/future');
 
 
 console.log('Update transcript with OCAS Applicant info');
-
-var authToken;
 
 // Setup the DDP connection
 var ddp = new DDP({
@@ -46,28 +44,77 @@ function ddpLoginCB(err) {
   //todo what if I can't connect
     throw err;
 
-  //var workers = Job.processJobs('student-transcript-in', 'updateTranscriptWithApplicant', {}, updateTranscript);
-  var workers = Job.processJobs('student-transcript-in', 'updateTranscriptWithApplicant', { payload: 20 }, updateTranscript);
+  var workers = Job.processJobs('student-transcript-in', 'updateTranscriptWithApplicant', {pollInterval:5000, prefetch: 2, workTimeout: 1*60*1000}, processJob);
+  //var workers = Job.processJobs('student-transcript-in', 'updateTranscriptWithApplicant', { payload: 20 }, processJobs);
 }
 
-/**
- * select distinct b.spriden_pidm,
- b.spriden_id,
- c.spbpers_birth_date,
- a.svroccc_term_code
- FROM svroccc a,
- spriden b,
- spbpers c
- WHERE a.svroccc_OCAS_APPL_NUM = '044760834'
- AND b.spriden_pidm = a.svroccc_pidm
- AND b.spriden_pidm = c.spbpers_pidm
- */
+function processJobs(jobs, cb) {
+  _.each(jobs, function(job) {
+    processJob(job);
+  });
+}
+
+function processJob(job, cb){
+  console.log("processing job "+job.doc._id+" data:"+JSON.stringify(job.data));
+  var transcriptId = job.data.transcriptId;
+  if (!transcriptId) {
+    job.log("Job data is invalid. transcriptId is required.");
+    //job.done();
+    cb();
+  } else {
+    ddp.call("getTranscript", [transcriptId], function(err, transcript) {
+      if (err) {
+        job.log("Failed to getTranscript.",err);
+        //job.fail();
+        cb();
+      } else {
+        console.log("getTranscript returned transcript with transmissionData:"+JSON.stringify(transcript.pescCollegeTranscript.TransmissionData));
+        var ocasApplicantId;
+        var err;
+        try {
+          ocasApplicantId = transcript.pescCollegeTranscript.CollegeTranscript.Student.Person.AgencyAssignedID;
+        } catch (error) {
+          err = error;
+        }
+        if (!ocasApplicantId) {
+          job.log("transcript missing AgencyAssignedID for student", err);
+          //job.done();
+          cb();
+        } else {
+          //todo: get applicant data for real
+          var applicant = getApplicant(ocasApplicantId);
+          ddp.call("setApplicant", [transcriptId, applicant], function(err, result){
+            console.log("setApplicant err:"+err);
+            console.log("setApplicant result:"+result);
+            //todo: improve error handling.
+            if (err) {
+              //job.fail();
+            } else {
+              //job.done();
+            }
+            cb();
+          });
+        }
+      }
+    });
+  }
+}
+
+var applicantQuery = "select distinct b.spriden_pidm,b.spriden_id,c.spbpers_birth_date,a.svroccc_term_code FROM svroccc a,spriden b,spbpers c WHERE a.svroccc_OCAS_APPL_NUM = :ocasApplicantId AND b.spriden_change_ind is null AND b.spriden_pidm = a.svroccc_pidm AND b.spriden_pidm = c.spbpers_pidm";
 function getApplicant(ocasApplicantId) {
-  var syncConnection = Future.wrapAsync(db.getConnection);
-  var connection = syncConnection({
+  //var syncConnection = Future.wrapAsync(db.getConnection);
+  db.getConnection({
     user: config.settings.oracleUserId,
     password: config.settings.oraclePassword,
     connectString: config.settings.oracleConnectString
+  }, function(err,connection){
+    if (err) throw err;
+    console.log("getApplicant called with applicantId: "+ ocasApplicantId);
+    connection.execute(applicantQuery, {ocasApplicantId:ocasApplicantId}, function(err, result){
+      console.log("error:"+err);
+      console.log("applicant result:"+JSON.stringify(result));
+      releaseConnection(connection);
+    })
   });
   //todo: hook up SQL....
   var applicant = {
@@ -82,31 +129,9 @@ function getApplicant(ocasApplicantId) {
   return applicant;
 }
 
-function updateTranscript(jobs) {
-  _.each(jobs, function(job) {
-    var transcriptId = job.data.transcriptId;
-    console.log("updateTranscript called for job "+job._id+", transcript "+transcriptId);
-    ddp.call("getTranscript", [transcriptId], function(err, transcript) {
-      if (err) {
-        job.fail();
-      } else {
-        var ocasApplicantId = transcript.pescCollegeTranscript.CollegeTranscript.Student.Person.AgencyAssignedID;
-        //todo: get applicant data for real
-        var applicant = getApplicant(ocasApplicantId);
-        ddp.call("setApplicant", [transcriptId, applicant], function(err, result){
-          console.log("setApplicant err:"+err);
-          console.log("setApplicant result:"+result);
-          //todo: improve error handling.
-          if (err) {
-            job.fail();
-          } else {
-            job.done();
-          }
-        });
-      }
-    });
+function releaseConnection(connection) {
+  connection.release(function(err){
+    if (err) console.error("Failed to release connection with error:"+err);
   });
 }
-
-
 
