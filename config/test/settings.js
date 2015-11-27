a0808990bdf4b357c87049274dd6fbc640f86202
Todd Hiles <todd.hiles@georgiancollege.ca>
2015-11-27 14:54:08 -0500

    tswebservices01 config settings, minus passwords.

---
+++ config/test/settings.js
@@ -0,0 +1,19 @@
+﻿// do not include in source control
+module.exports = {
+    userName : 'Partner\\uat.geor.marioncu',
+    passWord : 'georgianPassword',
+    grantType : 'password',
+    loginUrl: 'https://etms.uat.ocas.ca/api/auth/login',
+    transcriptsNoResponseUrl : 'https://etms.uat.ocas.ca/api/transcripts/no_response',
+    transcriptDetailUrl : 'https://etms.uat.ocas.ca/api/transcripts/',
+    requestsNoResponseUrl : 'https://etms.uat.ocas.ca/api/transcriptrequests/no_response',
+    transcriptRequestUrl : 'https://etms.uat.ocas.ca/api/transcriptrequests/',
+    sendTranscriptUrl : 'https://etms.uat.ocas.ca/api/transcriptrequests/',
+    ddpHost : 'jobcollection.georgiantest.com',
+    ddpPort : '80',
+    ddpUser : 'bob',
+    ddpPassword : 'bobspassword',
+    oracleUserId        : "bob",
+    oraclePassword      : "bobspassword",
+    oracleConnectString : "testrac01-vip.admin.georgianc.on.ca/GSB1"
+}
