c7bb85a93710514aa3daf6d9c5df49070ad13b95
Todd Hiles <todd.hiles@georgiancollege.ca>
2016-01-05 17:45:32 -0500

    Make Worker continually look for jobs to process. - changed login method - cleanup - todo: refactor to create separate jobs for each TranscriptRequest id.

--- getTranscriptRequestsFromOCASWorker.js
+++ getTranscriptRequestsFromOCASWorker.js
@@ -3,13 +3,10 @@
 var https = require('https');
 var request = require('request');
 var DDP = require('ddp');
-var DDPlogin = require('ddp-login');
 var Job = require('meteor-job');
 var config = require('app-config');
-var oracledb = require('oracledb');
 
 console.log('Start Her Up');
-var authToken;
 
 // Setup the DDP connection
 var ddp = new DDP({
@@ -36,70 +33,70 @@ function ddpLoginCB(err) {
         //todo what if I can't connect
         throw err;
     
-    //checkForTranscriptRequests();
-    setInterval(checkForTranscriptRequests, 30000);  
-
+    Job.processJobs('student-transcript-in', 'getTranscriptRequestsFromOCAS', {pollInterval:1*60*1000, workTimeout: 3*60*1000}, processJob);
 }
 
-function checkForTranscriptRequests() {
+function processJob(job, cb) {
+    console.log("processing job "+job.doc._id+" data:"+JSON.stringify(job.data));
     request.post(config.settings.loginUrl, { form: { Username: config.settings.userName, Password: config.settings.passWord, grant_type: config.settings.grantType } }, function (error, response, body) {
-        if (!error && response.statusCode == 200) {
-            var tokenInfo = JSON.parse(body);
-            authToken = tokenInfo.token_type + " " + tokenInfo.access_token;
-            
-            var options = {
-                url: config.settings.requestsNoResponseUrl,
-                headers: {
-                    'Authorization': authToken
-                }
-            };
-            
-            function getTranscriptDetails(error, response, body) {
-                if (!error && response.statusCode == 200) {
-                    var requestDetails = JSON.parse(body);
-                    console.log("write the transcript request details to the worker queue " + requestDetails.RequestID);
-                    // Create a job:
-                    var job = new Job('student-transcript-out', 'transcriptRequests', // type of job
-                    // Job data that you define, including anything the job
-                    // needs to complete. May contain links to files, etc...
-                    {
-                        requestId: requestDetails.RequestID,
-                        requestDetails: requestDetails.PESCXml
-                    }
-                    );
-                    job.priority('normal')
-                    .retry({
-                        retries: 5,
-                        wait: 15 * 60 * 1000
-                    })// 15 minutes between attempts 
-                    .save();               // Commit it to
-                }
-                return;
-            }
-            
-            function getTranscriptRequests(error, response, body) {
-                if (!error && response.statusCode == 200) {
-                    var info = JSON.parse(body);
-                    for (var i = 0; i < info.length; i++) {
-                        console.log(info[i].RequestID);
-                        
-                        //get the details
-                        var options2 = {
-                            url: config.settings.transcriptRequestUrl + info[i].RequestID,
-                            headers: {
-                                'Authorization': authToken
-                            }
-                        };
-                        request(options2, getTranscriptDetails);
-                    }                             
-                }
-            }
-            
-            request(options, getTranscriptRequests);
-        } else {
-            console.log("Got an error: ", error, ", status code: ", response.statusCode);
+        if (error || response.statusCode != 200) {
+            job.fail({task: "ocasAuthentication", exception: error, data: response})
+            cb();
+            return;
         }
-    });    
+        var tokenInfo = JSON.parse(body);
+        var authToken = tokenInfo.token_type + " " + tokenInfo.access_token;
+
+        var options = {
+            url: config.settings.requestsNoResponseUrl,
+            headers: {
+                'Authorization': authToken
+            }
+        };
+
+        function getTranscriptDetails(error, response, body) {
+            if (!error && response.statusCode == 200) {
+                var requestDetails = JSON.parse(body);
+                console.log("write the transcript request details to the worker queue " + requestDetails.RequestID);
+                // Create a job:
+                var job = new Job('student-transcript-out', 'transcriptRequests', // type of job
+                // Job data that you define, including anything the job
+                // needs to complete. May contain links to files, etc...
+                {
+                    requestId: requestDetails.RequestID,
+                    requestDetails: requestDetails.PESCXml
+                }
+                );
+                job.priority('normal')
+                .retry({
+                    retries: 5,
+                    wait: 15 * 60 * 1000
+                })// 15 minutes between attempts
+                .save();               // Commit it to
+            }
+            return;
+        }
+
+        function getTranscriptRequests(error, response, body) {
+            if (!error && response.statusCode == 200) {
+                var info = JSON.parse(body);
+                for (var i = 0; i < info.length; i++) {
+                    console.log(info[i].RequestID);
+
+                    //get the details
+                    var options2 = {
+                        url: config.settings.transcriptRequestUrl + info[i].RequestID,
+                        headers: {
+                            'Authorization': authToken
+                        }
+                    };
+                    request(options2, getTranscriptDetails);
+                }
+            }
+        }
+
+        request(options, getTranscriptRequests);
+    });
 }
 
 
