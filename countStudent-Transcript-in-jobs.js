d59a2c316c0a474f478610d03a87f18a6fbf88f6
Todd Hiles <todd.hiles@georgiancollege.ca>
2016-01-07 10:44:15 -0500

    Removed erroneous whitespace.

--- countStudent-Transcript-in-jobs.js
+++ countStudent-Transcript-in-jobs.js
@@ -1,13 +1,1 @@
-db.getCollection('student-transcript-in.jobs').aggregate(
-[
-{
-    $group: {
-        _id : { "type": "$type", "status": "$status" },
-        "count" : { "$sum": 1 }
-    }
-},
-{ "$sort": { "_id.type" : 1 } }
-]
-)
-
-
+db.getCollection('student-transcript-in.jobs').aggregate(    [        {            $group: {                _id : { "type": "$type", "status": "$status" },                "count" : { "$sum": 1 }            }        },        { "$sort": { "_id.type" : 1 } }    ])
