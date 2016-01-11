4c9f87ca261b135f23972688b07d9677b577da0f
Todd Hiles <todd.hiles@georgiancollege.ca>
2016-01-11 10:39:55 -0500

    Fix connection management issues found when observing process resources. - Use orawrap

--- updateTranscriptWithApplicantWorker.js
+++ updateTranscriptWithApplicantWorker.js
@@ -6,15 +6,23 @@ var config = require('app-config');
 var later = require('later');
 var _ = require('underscore');
 var db = require('oracledb');
+var orawrap = require('orawrap');
 var ddpLogin = require('./ddpLogin');
+var ddp;
 
 console.log('Update transcript with OCAS Applicant info');
 
-var ddp
-ddpLogin.onSuccess(function (ddpConnection){
-  ddp = ddpConnection;
-  Job.setDDP(ddpConnection);
-  Job.processJobs(config.settings.jobCollectionName, 'updateTranscriptWithApplicant', {pollInterval:5000, workTimeout: 1*60*1000}, processJob);
+orawrap.createPool(config.settings.dbConfig, function (err, pool){
+  if (err) {
+    console.log("createPool failed with err:"+err);
+    throw err;
+  }
+  ddpLogin.onSuccess(function (ddpConnection){
+    ddp = ddpConnection;
+    Job.setDDP(ddpConnection);
+    Job.processJobs(config.settings.jobCollectionName, 'updateTranscriptWithApplicant', {pollInterval:5000, workTimeout: 1*60*1000}, processJob);
+  });
+
 });
 
 function processJob(job, cb){
@@ -44,57 +52,39 @@ function processJob(job, cb){
         cb();
         return;
       }
-      db.getConnection({
-        user: config.settings.oracleUserId,
-        password: config.settings.oraclePassword,
-        connectString: config.settings.oracleConnectString
-      }, function(err,connection){
+      var applicantQuery = "select distinct b.spriden_pidm,b.spriden_id,c.spbpers_birth_date,a.svroccc_term_code, b.spriden_first_name, b.spriden_last_name FROM svroccc a,spriden b,spbpers c WHERE a.svroccc_OCAS_APPL_NUM = :ocasApplicantId AND b.spriden_change_ind is null AND b.spriden_pidm = a.svroccc_pidm AND b.spriden_pidm = c.spbpers_pidm order by a.svroccc_term_code";
+      orawrap.execute(applicantQuery, {ocasApplicantId:ocasApplicantId}, function(err, result){
         if (err) {
-          job.fail({task:"get DB connection", exception:err});
+          job.fail({task:"get applicant query", exception:err});
           cb();
           return;
         }
-        var applicantQuery = "select distinct b.spriden_pidm,b.spriden_id,c.spbpers_birth_date,a.svroccc_term_code, b.spriden_first_name, b.spriden_last_name FROM svroccc a,spriden b,spbpers c WHERE a.svroccc_OCAS_APPL_NUM = :ocasApplicantId AND b.spriden_change_ind is null AND b.spriden_pidm = a.svroccc_pidm AND b.spriden_pidm = c.spbpers_pidm order by a.svroccc_term_code";
-        connection.execute(applicantQuery, {ocasApplicantId:ocasApplicantId}, function(err, result){
-          releaseConnection(connection);
-          if (err) {
-            job.fail({task:"get applicant query", exception:err});
-            cb();
-            return;
-          }
-          //Use earliest term code when more than one match; order is essential!
-          var firstMatch = result.rows[0];
-          if (!firstMatch) {
-            job.fail({task:"Find applicant", data: ocasApplicantId});
-            cb();
-            return;
-          }
-          var applicant = {
-            applicantId: ocasApplicantId,
-            pidm: firstMatch[0],
-            studentId: firstMatch[1],
-            birthDate: firstMatch[2],
-            termCode: firstMatch[3],
-            firstName: firstMatch[4],
-            lastName: firstMatch[5]
-          };
+        //Use earliest term code when more than one match; order is essential!
+        var firstMatch = result.rows[0];
+        if (!firstMatch) {
+          job.fail({task:"Find applicant", data: ocasApplicantId});
+          cb();
+          return;
+        }
+        var applicant = {
+          applicantId: ocasApplicantId,
+          pidm: firstMatch[0],
+          studentId: firstMatch[1],
+          birthDate: firstMatch[2],
+          termCode: firstMatch[3],
+          firstName: firstMatch[4],
+          lastName: firstMatch[5]
+        };
 
-          ddp.call("setApplicant", [transcriptId, applicant], function(err, result){
-            if (err) {
-              job.fail({task:"setApplicant",exception:err, data: {transcriptId:transcriptId, applicant:applicant}});
-            }
-            job.done();
-            cb();
-          });
-        })
-      });
+        ddp.call("setApplicant", [transcriptId, applicant], function(err, result){
+          if (err) {
+            job.fail({task:"setApplicant",exception:err, data: {transcriptId:transcriptId, applicant:applicant}});
+          }
+          job.done();
+          cb();
+        });
+      })
     });
   }
-}
-
-function releaseConnection(connection) {
-  connection.release(function(err){
-    if (err) console.error("Failed to release connection with error:"+err);
-  });
 }
 
