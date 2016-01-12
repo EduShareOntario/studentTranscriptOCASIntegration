4549fde389eb847fa1c13ff297ef5d44be2f7075
Todd Hiles <todd.hiles@georgiancollege.ca>
2016-01-12 11:33:08 -0500

    Add stop and start scripts for all TranscriptWorker services.

--- private/config/status.ps1
+++ private/config/status.ps1
@@ -1,1 +1,1 @@
-get-service | foreach-object {if ($_.name.startsWith('Transcript')) { write-output ($_.name + ', status:'+$_.status); } }
+get-service | foreach-object {if ($_.name.startsWith('TranscriptWorker')) { write-output ($_.name + ', status:'+$_.status); } }
