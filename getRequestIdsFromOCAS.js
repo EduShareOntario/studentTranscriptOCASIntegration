0d25dd2c5042c45d5a16cde7711d49859b2339c4
Todd Hiles <todd.hiles@georgiancollege.ca>
2016-01-18 17:11:37 -0500

    By-pass creating redundant jobs.

--- getRequestIdsFromOCAS.js
+++ getRequestIdsFromOCAS.js
@@ -6,12 +6,15 @@ var config = require('app-config');
 var later = require('later');
 var Fiber = require('fibers');
 var Future = require('fibers/future');
+var _ = require('underscore');
 var ddpLogin = require('./ddpLogin');
 var ocasLogin = require('./ocasLogin');
 
 function start(processJobConfig, createJobConfig) {
   console.log('Start-up');
-  ddpLogin.onSuccess(function(ddp) {
+  var ddpClient;
+  ddpLogin.onSuccess(function (ddp) {
+    ddpClient = ddp;
     Job.setDDP(ddp);
     Job.processJobs(processJobConfig.root, processJobConfig.type, processJobConfig.options, processJob);
   });
@@ -28,40 +31,56 @@ function start(processJobConfig, createJobConfig) {
   }
 
   function processJob(job, cb) {
-    ocasLogin.onLogin(function(err, authToken){
+    ocasLogin.onLogin(function (err, authToken) {
       if (err) {
-        job.fail({task:"ocasAcquireAuthToken", exception:err});
+        job.fail({task: "ocasAcquireAuthToken", exception: err});
         cb();
         return;
       }
       function createJobs(requests) {
         // This task will start saving all the jobs and then wait for them all to complete
         // before deciding on the success/failure.
-        Future.task(function () {
-          var errors = [];
-          var requestIds = requests.map( function(request) { return request.RequestID;});
-          var jobs = requestIds.map(function (requestId) { return createJob(requestId);});
-          var jobSaveFutures = jobs.map(function (j){ return Future.wrap(j).saveFuture();});
-          // Wait for all the saves to complete.
-          // get() returns result/jobId in our case or throws and error.
-          // We want to remember the Errors but not terminate processing!
-          var savedJobIds = jobSaveFutures.map(function (saveFuture) {
-            try {
-              saveFuture.wait();
-              var jobId = saveFuture.get()
-              return jobId;
-            } catch (err) {
-              errors.push({task: "createJob", exception: err});
-            }
-          });
-          if (errors.length > 0) {
-            job.fail({errors: errors});
-          } else {
-            job.done({savedJobs: savedJobIds});
+        var requestIds = requests.map(function (request) {
+          return request.RequestID;
+        });
+        ddpClient.call("findRedundantJobs", [createJobConfig.root, {"data.ocasRequestId": {$in: requestIds}, status: {$ne: "failed"}}, {"data.ocasRequestId": 1}], function (err, redundantJobs) {
+          if (err) {
+            job.fail({task: "findRedundantJobs", exception: err});
+            cb();
+            return;
           }
-          cb();
-        }).detach();
+          var redundantIds = _.map(redundantJobs, function(job) { return job.data.ocasRequestId; });
+          var newRequestIds = _.difference(requestIds, redundantIds)
+          var jobs = newRequestIds.map(function (requestId) {
+            return createJob(requestId);
+          });
+          Future.task(function () {
+            var errors = [];
+            var jobSaveFutures = jobs.map(function (j) {
+              return Future.wrap(j).saveFuture();
+            });
+            // Wait for all the saves to complete.
+            // get() returns result/jobId in our case or throws and error.
+            // We want to remember the Errors but not terminate processing!
+            var savedJobIds = jobSaveFutures.map(function (saveFuture) {
+              try {
+                saveFuture.wait();
+                var jobId = saveFuture.get()
+                return jobId;
+              } catch (err) {
+                errors.push({task: "createJob", exception: err});
+              }
+            });
+            if (errors.length > 0) {
+              job.fail({errors: errors});
+            } else {
+              job.done({savedJobs: savedJobIds});
+            }
+            cb();
+          }).detach();
+        });
       }
+
       var ocasRequestOptions = {
         url: processJobConfig.ocasUrl,
         headers: {
@@ -70,7 +89,12 @@ function start(processJobConfig, createJobConfig) {
       };
       request(ocasRequestOptions, function (error, response, body) {
         if (error || response.statusCode != 200) {
-          job.fail({task:"get $($ocasRequestOptions.ocasUrl)", exception:error, responseStatus: response.statusCode, data: body});
+          job.fail({
+            task: "get $($ocasRequestOptions.ocasUrl)",
+            exception: error,
+            responseStatus: response.statusCode,
+            data: body
+          });
           cb();
           return;
         }
