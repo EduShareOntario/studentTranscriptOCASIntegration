edb1c1e2e3259deafac58dc3c2acd23639b20e8e
moonrockfamily <2toddhiles@gmail.com>
2016-11-03 14:14:25 -0400

    Added query for finding jobs by OCAS Request Ids

--- scripts/mongo/transcriptJob_AdhocQueries.js
+++ scripts/mongo/transcriptJob_AdhocQueries.js
@@ -8,3 +8,13 @@ db.getCollection('student-transcript.jobs').find({status: 'waiting'})
 db.getCollection('student-transcript.jobs').find({_id: 'ghq6bXLMiRfPnqitE'})
 
 db.getCollection('student-transcript.jobs').find({_id: 'Rx2A2qfYwNjTtoRcq'})
+
+//find jobs for given OCAS request ids
+ocasRequestIds= ["2016092701015"]
+db.getCollection('student-transcript.jobs').find({ 
+    $or: [
+    {"data.ocasRequestId": {$in:ocasRequestIds}},
+    {"data.requestId": {$in:ocasRequestIds}},
+    {"ocasRequestId": {$in:ocasRequestIds}},
+    ]
+})
